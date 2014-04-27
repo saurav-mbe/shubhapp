9b41ac5ce8e5443c62b70a8b20409274476d8151
Saurav <saurav.mbe@gmail.com>
2014-04-27 12:19:55 +0530

    more changes

--- platforms/android/ant-gen/R.java.d
+++ platforms/android/ant-gen/R.java.d
@@ -6,9 +6,4 @@ C:\projects\app1\platforms\android\res\drawable-mdpi\icon.png \
 C:\projects\app1\platforms\android\res\drawable-xhdpi\icon.png \
 C:\projects\app1\platforms\android\res\values\strings.xml \
 C:\projects\app1\platforms\android\res\xml\config.xml \
-C:\projects\app1\platforms\android\ant-build\res\drawable\icon.png \
-C:\projects\app1\platforms\android\ant-build\res\drawable-hdpi\icon.png \
-C:\projects\app1\platforms\android\ant-build\res\drawable-ldpi\icon.png \
-C:\projects\app1\platforms\android\ant-build\res\drawable-mdpi\icon.png \
-C:\projects\app1\platforms\android\ant-build\res\drawable-xhdpi\icon.png \
 C:\projects\app1\platforms\android\ant-build\AndroidManifest.xml \
